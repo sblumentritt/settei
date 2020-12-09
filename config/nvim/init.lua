38112ad8af42e0ce9598887d91f4b0f2b19b8305
Sebastian Blumentritt <blumentritt.sebastian@protonmail.com>
2020-12-09 08:00:49 +0100

    Rewrite the neovim startup script in lua

---
+++ config/nvim/init.lua
@@ -0,0 +1,35 @@
+-- --------------------------------------
+-- entry point for neovim
+-- --------------------------------------
+
+-- define value for <leader> as early as possible
+-- --------------------------------------
+vim.g.mapleader = ','
+
+-- save paths as vimscript environment variables
+-- --------------------------------------
+vim.env.neovim_data_dir = vim.fn.stdpath('data')..'/site'
+vim.env.neovim_plugin_dir = vim.env.neovim_data_dir..'/pack/packer/start'
+
+-- disable some standard plugins
+-- --------------------------------------
+vim.g.loaded_gzip = 1
+
+vim.g.loaded_tar = 1
+vim.g.loaded_tarPlugin = 1
+
+vim.g.loaded_zip = 1
+vim.g.loaded_zipPlugin = 1
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- load plugins configuration file
+-- --------------------------------------
+require('plugins')
+
+-- always install missing plugins when starting
+vim.cmd('PackerInstall')
+
+-- NOTE: is this still needed?
+vim.cmd('filetype plugin indent on')
