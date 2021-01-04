9eee4095fbfc3937f24ede7b479b04a13a4211c8
Sebastian Blumentritt <blumentritt.sebastian@protonmail.com>
2021-01-04 09:18:30 +0100

    Add helper function to create augroups

--- config/nvim/lua/core/utils.lua
+++ config/nvim/lua/core/utils.lua
@@ -5,4 +5,18 @@ function utils.keymap(type, key, value)
     vim.fn.nvim_set_keymap(type, key, value, {noremap = true, silent = true});
 end
 
+function utils.create_augroups(definitions)
+    for group_name, definition in pairs(definitions) do
+        vim.cmd('augroup '..group_name)
+        vim.cmd('autocmd!')
+
+        for _, def in ipairs(definition) do
+            local command = table.concat(vim.tbl_flatten{'autocmd', def}, ' ')
+            vim.cmd(command)
+        end
+
+        vim.cmd('augroup END')
+    end
+end
+
 return utils
