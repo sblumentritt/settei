284aa4a635490cf0a7bc1c27b2444f45b0fd1283
Sebastian Blumentritt <blumentritt.sebastian@protonmail.com>
2021-01-04 15:16:11 +0100

    Revert "Remove include-what-you-use related configurations"
    This reverts commit 1b1e460ec1a4cabcdbd61db5eff24fc0f4802997.

--- script/setup/configure.sh
+++ script/setup/configure.sh
@@ -82,6 +82,7 @@ file_setup() {
     ln -sf "${CONFIG_BASE_PATH}/config/nvim" $HOME/.config/
     ln -sf "${CONFIG_BASE_PATH}/config/tmux" $HOME/.config/
     ln -sf "${CONFIG_BASE_PATH}/config/mako" $HOME/.config/
+    ln -sf "${CONFIG_BASE_PATH}/config/iwyu" $HOME/.config/
     ln -sf "${CONFIG_BASE_PATH}/config/qt5ct" $HOME/.config/
     ln -sf "${CONFIG_BASE_PATH}/config/Kvantum" $HOME/.config/
     ln -sf "${CONFIG_BASE_PATH}/config/gtk-3.0" $HOME/.config/
@@ -242,7 +243,7 @@ external_packages() {
 
     # iterate over custom packages
     # --------------------------------------
-    local external_packages="neovim,sumneko_lua_lsp,shellcheck,pop-gtk-theme"
+    local external_packages="neovim,sumneko_lua_lsp,shellcheck,pop-gtk-theme,iwyu"
 
     for package in $external_packages; do
         cp -r "${CONFIG_BASE_PATH}/pkgbuild/${package}" "./${package}"
